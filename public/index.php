d8a149fa5c39c8dd90791c4ca2b4dec70756077f
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-03-18 17:35:39 +0900

    Move to public directory.

--- public/index.php
+++ public/index.php
@@ -1,12 +1,12 @@
 <?php
-set_include_path(__DIR__ . '/vendor/timed-text/src' . PATH_SEPARATOR . get_include_path());
-require_once __DIR__ . '/vendor/silex.phar';
+set_include_path(__DIR__ . '/../vendor/timed-text/src' . PATH_SEPARATOR . get_include_path());
+require_once __DIR__ . '/../vendor/silex.phar';
 require_once 'TimedText.php';
 
 $app = new Silex\Application;
 $app->register(new Silex\Provider\TwigServiceProvider, array(
-    'twig.path'       => __DIR__ . '/views',
-    'twig.class_path' => __DIR__ . '/vendor/twig/lib',
+    'twig.path'       => __DIR__ . '/../views',
+    'twig.class_path' => __DIR__ . '/../vendor/twig/lib',
 ));
 
 $app->before(function () use ($app) {
