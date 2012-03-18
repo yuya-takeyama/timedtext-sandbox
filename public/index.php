6ae2f14670e7cccc56e3f8ad12c6dc7bd6d7da3e
Yuya Takeyama <sign.of.the.wolf.pentagram@gmail.com>
2012-03-18 18:10:10 +0900

    Add TimedText display.

--- public/index.php
+++ public/index.php
@@ -3,18 +3,46 @@ set_include_path(__DIR__ . '/../vendor/timed-text/src' . PATH_SEPARATOR . get_in
 require_once __DIR__ . '/../vendor/silex.phar';
 require_once 'TimedText.php';
 
+use \Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\Cookie;
+
+define('DEFAULT_TEXT', <<<__TEXT__
+{before 2012-03-18 00:00}
+この文字列は 2012-03-18 00:00 まで表示されます.
+{/before}
+この文字列は常に表示されます.
+{after 2012-03-20 00:00}
+この文字列は 2012-03-20 00:00 以降に表示されます.
+{/after}
+__TEXT__
+);
+
 $app = new Silex\Application;
+$app['debug'] = true;
 $app->register(new Silex\Provider\TwigServiceProvider, array(
     'twig.path'       => __DIR__ . '/../views',
     'twig.class_path' => __DIR__ . '/../vendor/twig/lib',
 ));
 
 $app->before(function () use ($app) {
+    $app->response = new Response;
     $app['twig']->addGlobal('layout', $app['twig']->loadTemplate('layout.twig'));
+    if (! $app['request']->cookies->has('text')) {
+        $app->response->headers->setCookie(new Cookie('text', DEFAULT_TEXT));
+        $app->text = DEFAULT_TEXT;
+    } else {
+        $app->text = $app['request']->cookies->get('text');
+    }
 });
 
 $app->get('/', function () use ($app) {
-    return $app['twig']->render('index.twig');
+    $app->response->setContent(
+        $app['twig']->render('index.twig', array(
+            'raw_text'       => $app->text,
+            'converted_text' => TimedText::convert($app->text),
+        ))
+    );
+    return $app->response;
 });
 
 $app->run();
